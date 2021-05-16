36dfea8e2c55b1efbf8c4f14e7c079072b07b4b6
João Santos <joaoo.santoss.98@gmail.com>
2021-05-16 12:54:52 +0100

    Criadas funçoes CRUD para as tabelas da base de dados

--- app/src/main/java/com/example/livros/Tabelalivros.kt
+++ app/src/main/java/com/example/livros/Tabelalivros.kt
@@ -1,5 +1,7 @@
 package com.example.livros
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
@@ -17,6 +19,24 @@ class Tabelalivros (db: SQLiteDatabase) {
                 TabelaCategorias.NOME_TABELA +
                 ")")
     }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(TabelaCategorias.NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(TabelaCategorias.NOME_TABELA, values, whereClause, whereArgs)
+
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(TabelaCategorias.NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(colums: Array<String>, selection: String, selectionArgs: Array<String>, groupBy: String, having: String, orderBy: String): Cursor? {
+        return db.query(TabelaCategorias.NOME_TABELA, colums, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
 
     companion object{
         const val NOME_TABELA = "livros"
